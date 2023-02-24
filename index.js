bc2311418d849c4b8de34bec04d78dc4355613be
Shivva Kumar <121939857+Shivvak@users.noreply.github.com>
2023-02-24 15:36:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+
+// let name =  function(){
+//   console.log("hey wsup?");
+// }
+// name();
+
+
+
+// console.log("hi shivva")
+
+
+
+// let der = isNaN("siva")
+// console.log("is it Nan", der);
+
+// let der1 = isNaN("20")
+// console.log("is it Nan", der1);
+
+// console.log(typeof(der));
+
+// console.log(typeof(name));
+
+
+// console.log(1 && 0)
+// console.log(1 && 1)
+
+
+// console.log(1 || 1)
+// console.log(1 || 0)
+
+
+
+
+
+
+let A =100;
+// let B= 200;
+// console.log(A<B && B<0)
+// console.log(A<B && B>0)
+
+// console.log(A>B || B<0)
+// console.log(A<B || B>0)
+// console.log(A<B || B<0)
+
+
+
+
+// day5
+
+
+// const  bikes =["k", "g,", "u", 'k', 's', 'h', 'i', 'v', 'a']
+// console.log(bikes.splice(0,2,'r'));
+// // console.log(bikes.slice());
+
+
+
+
+let num= 6;
+let pattern ="";
+for (let i=1;i<=num;i++){
+    for (let j=1; j<=num-i; j++){
+        pattern += '*';
+    }
+    pattern += "\n"
+}
+
+console.log(pattern)
+
+
